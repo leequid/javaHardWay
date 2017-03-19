2f6a2d1e23b9f0eb49e42b00c3a9a2eede5b389f
leequid <1988elee@gmail.com>
2017-03-18 18:51:59 -0600

    add exercise1

---
+++ FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+		public static void main( String [] args ) {
+			System.out.println("I, Eric Lee, am determined to learn how to code.");
+			System.out.println("Today's date is March 11, 2017");
+		}
+		
+}
